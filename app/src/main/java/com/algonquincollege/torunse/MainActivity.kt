43e219590ec2eb7cdf89e346f1399dfafe0e17ca
Eric Torunski <etorunski@gmail.com>
2025-09-23 11:29:07 -0400

    worked on sensors

--- app/src/main/java/com/algonquincollege/torunse/MainActivity.kt
+++ app/src/main/java/com/algonquincollege/torunse/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.algonquincollege.torunse
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import  android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,7 +14,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.algonquincollege.torunse.ui.theme.MyAndroidLabsTheme
 
@@ -24,8 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyAndroidLabsTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DisplayLighting(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -35,9 +43,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun DisplayLighting( modifier: Modifier = Modifier) {
+
+    var sensorManager = LocalContext.current.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    var value = remember{  mutableStateOf( 0.0f )  }//makes it not garbage collected
+
+
+    //might return null if not on phone
+    val lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+    val sensorListener = object: SensorEventListener{
+
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+        }
+        override fun onSensorChanged(event: SensorEvent?) {
+            value.value = event!!.values[0]   //non-null assertion, array of size 1, or size 3
+        }
+    }
+                                                //null
+    sensorManager.registerListener(sensorListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+
     Text(
-        text = "Hello, friend $name!",
+        text = "The lighting is: ${value.value}",
         modifier = modifier
     )
 }
@@ -46,6 +74,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyAndroidLabsTheme {
-        Greeting("Android")
+        DisplayLighting()
     }
 }
