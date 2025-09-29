71930b9cc9420b9da62d06e37e106bfe7a88fc40
Eric Torunski <etorunski@gmail.com>
2025-09-29 10:05:40 -0400

    worked on sensors

--- app/src/main/java/com/algonquincollege/torunse/MainActivity.kt
+++ app/src/main/java/com/algonquincollege/torunse/MainActivity.kt
@@ -9,8 +9,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,7 +24,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.algonquincollege.torunse.ui.theme.MyAndroidLabsTheme
 
 //https://developer.android.com/develop/ui/compose/tooling/iterative-development
@@ -78,14 +85,11 @@ fun DisplayLighting(lightingValue: Float, modifier: Modifier = Modifier) {
         verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
         ) {
         Text(
-            text = "The lighting is: ${value.value}",
+            text = "The lighting is: ${lightingValue}",
             modifier = modifier,
             fontSize = 20.sp,
 
             )
-        TextField( value = input.value, onValueChange = {
-            input.value = it  //it is a name for standard parameter
-        } )
         Icon(painter=painterResource(R.drawable.beach), contentDescription = "a beach")
         Image(painterResource( R.drawable.beach ),
             contentDescription = "A picture of ??",
