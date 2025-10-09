953608c1deb1e5983d343707f843ebb03b73a196
Eric Torunski <etorunski@gmail.com>
2025-10-09 08:19:49 -0400

    bluetooth code

--- app/src/main/java/com/algonquincollege/torunse/MainActivity.kt
+++ app/src/main/java/com/algonquincollege/torunse/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.algonquincollege.torunse
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattServer
+import android.bluetooth.BluetoothGattServerCallback
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import  android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
+import android.os.ParcelUuid
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,60 +33,100 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.algonquincollege.torunse.ui.theme.MyAndroidLabsTheme
+import java.util.UUID
 
 //https://developer.android.com/develop/ui/compose/tooling/iterative-development
 
 
 class MainActivity : ComponentActivity() {
 
+    private var bluetoothManager:BluetoothManager? = null
+    private var bluetoothAdapter: BluetoothAdapter? = null
+    private var gattServer : BluetoothGattServer? = null
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        var sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as? BluetoothManager
+        if(bluetoothManager != null){
+            bluetoothAdapter = bluetoothManager?.getAdapter()
+        }
 
-        //might return null if not on phone
-        val lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        val gattCallbacks = object: BluetoothGattServerCallback() { } //We will implement the inherited functions one at a time and understand what each one does
+        val bluetoothLeAdvertiser = bluetoothAdapter?.getBluetoothLeAdvertiser()
 
-        var sensorListener : SensorEventListener? = null
+        val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+                    isGranted ->
+                if (isGranted.values.all { it  == true}) {
+                    gattServer = bluetoothManager?.openGattServer(this, gattCallbacks )
 
-        setContent {
 
-             var value = remember { mutableStateOf(0.0f) }
+                    val settings =  AdvertiseSettings.Builder()
+                        .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                        .setTxPowerLevel(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                        .setConnectable(true)
+                        .build()
 
+                    val advertisingData = AdvertiseData.Builder()
+                        .addServiceUuid(ParcelUuid(UUID.fromString("0000180D-0000-1000-8000-00805F9B34FB")))
+                        .setIncludeDeviceName(true)
+                        .build()
 
-            if(sensorListener == null) {
-                sensorListener = object : SensorEventListener {
+                    val advertiseCallback = object: AdvertiseCallback() {
+                        override fun onStartSuccess(settingsInEffect: AdvertiseSettings?) {
+                            super.onStartSuccess(settingsInEffect)
+                            Log.d(TAG, "Advertising started successfully")
+                        }
 
-                    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-
+                        override fun onStartFailure(errorCode: Int) {
+                            super.onStartFailure(errorCode)
+                            if(errorCode == ADVERTISE_FAILED_INTERNAL_ERROR)
+                            Log.e(TAG, "Advertising failed with error code $errorCode")
+                        }
                     }
 
-                    override fun onSensorChanged(event: SensorEvent?) {
-                        value.value =
-                            event!!.values[0]   //non-null assertion, array of size 1, or size 3
-                    }
+                    bluetoothLeAdvertiser?.startAdvertising(settings, advertisingData, advertiseCallback);
+
+                } else {
+                    val i = 0
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
                 }
-                //null
-                sensorManager.registerListener(sensorListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
             }
 
+        setContent {
             MyAndroidLabsTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    DisplayLighting(value.value,
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)){
+                        Button(onClick = {
+
+
+                        },){
+                            Text("Click Me")
+                        }
+                    }
                 }
             }
         }
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) //31 or more
+        {
+            requestPermissionLauncher.launch(arrayOf(Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_ADVERTISE))
+        }
+        else //30 or less
+        {
+            requestPermissionLauncher.launch(arrayOf(Manifest.permission.BLUETOOTH))
+        }
+
     }
 
 }
