011ae3cd7b5ee1b6d1ed572f16d08ac00bd0f709
Eric Torunski <etorunski@gmail.com>
2025-11-17 10:28:29 -0500

    added how to make a string from UUID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ android {
 }
 
 dependencies {
+
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/algonquincollege/torunse/MainActivity.kt
+++ app/src/main/java/com/algonquincollege/torunse/MainActivity.kt
@@ -37,10 +37,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import com.algonquincollege.torunse.ui.theme.MyAndroidLabsTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -235,14 +238,19 @@ class MainActivity : ComponentActivity() {
             }
 
         setContent {
+
+            var stringUUID = remember{ mutableStateOf("") }
             MyAndroidLabsTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Column(modifier = Modifier.padding(innerPadding)){
                         Button(onClick = {
+                            stringUUID.value = ParcelUuid(UUID.randomUUID()).toString()
 
+                        }){ Text("Click Me") }
 
-                        },){
-                            Text("Click Me")
+                        if(stringUUID.value.isNotEmpty())
+                        {
+                            AsyncImage(model="http://api.qrserver.com/v1/create-qr-code/?data=abcde", contentDescription = "A QR code")
                         }
                     }
                 }
